e00fe45c032b632c4ad19363a74e102be0017b76
Alex Torres <89925046+AlexT0701@users.noreply.github.com>
2021-12-10 05:52:26 -0600

    Add files via upload

---
+++ hourly Login/Homepage/News/news.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HashRen | Corporate News</title>
+  <link rel="stylesheet" href="news.css">
+</head>
+
+<body>
+<div class="header">
+  <a href="homepage.php" class="logo">HashRen Manage</a>
+  <div class="header-right">
+    <a href="about.php">About</a>
+    <a href="loginTest.php">Logout</a>
+  </div>
+</div>
+
+<div class="newsHeader">
+  <h2>Corporate News</h2>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>Presentation Time!</h2>
+      <h5>Hashren Manage is finally presented to peers., Dec 7, 2021</h5>
+      <img src="presentation.png" alt="present" style="Width:100%; border:1px solid black">
+		<p>Hey There!</p>
+		<p>
+			On this wonderful day, the Syntax Terminators have demoed their finished Hashren product
+			to the peers of their class!
+		</p>
+    </div>
+    <div class="card">
+      <h2>Company Creation</h2>
+      <h5>Hashren is born and Employers are at Our Heels!, Nov 2, 2021</h5>
+      <img src="hashman.png" alt="hashman" style="Width:100%; border:1px solid black">
+		<p>Hello There,</p>
+		<p>
+			On this miraculous day, Hashren Manage has been born and has been deemed presentable!
+			From starting as just a mere idea to turning into a full released product, Hashren Manage
+			has outshined many of its competitors with its new and improved HR integration!
+		</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>Employee of the Month!</h2>
+      <img src="test.png" alt="gogeta" style="Width:100%; border:1px solid black">
+      <p>
+		December employee of the month has been awarded to <b><u>Dr. Dogdu</u></b>! Congradulations
+		on this accomplishment and rising to be the best!
+	  </p>
+    </div>
+    <div class="card">
+      <h3>Popular Post</h3>
+      <img src="popular1.png" alt="popular1" style="Width:100%; border:1px solid black">
+      <img src="popular2.png" alt="popular2" style="Width:100%; border:1px solid black">
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
+</body>
+</html>
