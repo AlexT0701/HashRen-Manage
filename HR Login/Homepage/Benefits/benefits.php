82637fb8c882af8ae4581d9b81573df75c7cf2fd
Alex Torres <89925046+AlexT0701@users.noreply.github.com>
2021-12-10 05:08:18 -0600

    Add files via upload

---
+++ HR Login/Homepage/Benefits/benefits.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HashRen Login</title>
+  <link rel="stylesheet" href="hrHomepage.css">
+  <link rel="stylesheet" href="benefits.css">
+</head>
+
+<body>
+<div class="header">
+  <a href="hrHomepage.php" class="logo">HashRen Manage</a>
+  <div class="header-right">
+    <a href="hrAbout.php">About</a>
+    <a href="loginTest.php">Logout</a>
+  </div>
+</div>
+
+<div class="mainBody">
+	<h1>Employee Benefits</h1>
+	<p class="subHeaderPara">
+	The following is a summary of benefits provided to employees of Hashren, Inc. 
+	All new employees will receive a detailed packet of information about eligibility, 
+	enrollment and other important information upon reporting to work at the appropriate location. 
+	</p>
+	
+	<h2 class="headers">Health Benefits</h2>
+	<h3 class="subHeaders">HEALTH INSURANCE</h3>
+	<p class="subHeaderPara">
+	Hashren offers market-leading and comprehensive medical/prescription drug plan benefits. 
+	Coverage is available for all full-time employees and their eligible dependents (e.g., spouse, children).
+	</p>
+	<h3 class="subHeaders">FLEXIBLE SPENDING ACCOUNT</h3>
+	<p class="subHeaderPara">
+	Health and dependent care flexible spending accounts are available to employees. This voluntary benefit 
+	provides employees with a tax advantage that can help pay health care and dependent care expenses on a pre-tax basis.
+	</p>
+	
+	<h2 class="headers">Income Protection Benefits</h2>
+	<h3 class="subHeaders">LONG-TERM DISABILITY INSURANCE</h3>
+	<p class="subHeaderPara">
+	Hashren provides LTD insurance to qualified employees which gives them the security of knowing that if they become 
+	disabled, replacement income is available to help them maintain their present lifestyle. This benefit is provided 
+	at no cost to employees.
+	</p>
+	<h3 class="subHeaders">OCCUPATIONAL ILLNESS & INJURY</h3>
+	<p class="subHeaderPara">
+	Hashren, Inc. provides Workers' Compensation coverage to protect employees from the loss of wages and provide 
+	medical treatment service and cost coverage due to a job-related injury or illness.
+	</p>
+	
+	<h2 class="headers">Survivor Benefits</h2>
+	<h3 class="subHeaders">LIFE INSURANCE AND ACCIDENTAL DEATH & DISMEMBERMENT INSURANCE</h3>
+	<p class="subHeaderPara">
+	A life insurance policy is provided at no cost to all full-time employees at the rate of 1.5 (150%) of the employee's 
+	qualified annual earnings. An accidental death & dismemberment insurance policy is also provided at no cost to all full-time 
+	employees.
+	</p>
+	
+	<h2 class="headers">Dental Benefits</h2>
+	<h3 class="subHeaders">VOLUNTARY DENTAL INSURANCE</h3>
+	<p class="subHeaderPara">
+	Hashren offers a comprehensive voluntary dental benefit plan.  Coverage is available for all full-time employees and 
+	their eligible dependents (e.g., spouse, children).
+	</p>
+	
+	<h2 class="headers">Retirement Benefits</h2>
+	<h3 class="subHeaders">401(K)</h3>
+	<p class="subHeaderPara">
+	Hashren, Inc. provides a 3% contribution of qualified wages to company employees based on certain eligibility requirements.
+	</p>
+	
+	<h2 class="headers">Paid Time Off</h2>
+	<h3 class="subHeaders">PAID TIME OFF (PTO) BANK</h3>
+	<p class="subHeaderPara">
+	Hashren provides a generous paid time off (PTO) bank benefit to hourly/non-exempt full-time employees with paid time away 
+	from work that can be used for a variety of reasons such as vacation, personal time, personal illness/injury, jury duty, 
+	funeral leave, care of dependents, etc. These employees are eligible for 2 weeks of PTO after 1 year of service, 3 weeks 
+	after 5 years of service, and 4 weeks after 15 years of service. The PTO benefit also includes a generous extended paid 
+	leave provision for personal illness or injury according to policy requirements.
+	</p>
+	<h3 class="subHeaders">PAID VACATION & SICK LEAVE</h3>
+	<p class="subHeaderPara">
+	Salaried/exempt and salaried/non-exempt (sales) employees are eligible for traditional paid vacation benefits, e.g., 
+	2 weeks of paid vacation upon hire date and generous paid sick leave benefits according to company policies.
+	</p>
+	<h3 class="subHeaders">HOLIDAYS</h3>
+	<p class="subHeaderPara">
+	All full-time employees are eligible for 6 paid holidays each calendar year (New Year's Day, Memorial Day, Independence 
+	Day, Labor Day, Thanksgiving and Christmas Day) and any additional holidays approved by management.
+	</p>
+	
+	<h2 class="headers">Individual & Family Well Being</h2>
+	<h3 class="subHeaders">EMPLOYEE ASSISTANCE PROGRAM (EAP)</h3>
+	<p class="subHeaderPara">
+	Employees and their immediate family members are provided up to 8 counseling sessions per problem per year to help resolve 
+	personal issues such as stress, emotional/mental problems, marital/family problems, financial difficulties, alcohol/drug abuse, 
+	work related difficulties, and grief/loss issues. Sessions are provided at no cost to the employee or his/her immediate family member.
+	</p>
+	
+</div>
+
+</body>
