85d57ffc74d1509bb5820e677a32f8eb8b6490de
Alex Torres <89925046+AlexT0701@users.noreply.github.com>
2021-12-10 05:29:06 -0600

    Add files via upload

---
+++ HR Login/Homepage/Time Management/Edit Time/editTime.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Edit Tme Schedule</title>
+  <link rel="stylesheet" href="hrHomepage.css">
+  <link rel="stylesheet" href="editEmp.css">
+</head>
+
+<body>
+<div class="header">
+  <a href="hrHomepage.php" class="logo">HashRen Manage</a>
+  <div class="header-right">
+    <a href="hrAbout.php">About</a>
+    <a href="loginTest.php">Logout</a>
+  </div>
+</div>
+
+<p></p>
+<p></p>
+
+<body>
+  <fieldset>
+    <legend>Employee Form</legend>
+    <form name="addEmployee" method="post">
+      <p>
+        <label for="name">Employee Name</label>
+        <input type="text" name="txtName" id="txtId" placeholder="Employee Name" >
+      </p>
+      <p>
+        <label for="sun">Sunday Availability </label>
+        <input type="text" name="txtSun" id="txtSun" placeholder="Work Hours" >
+      </p>
+      <p>
+        <label for="mon">Monday Availability</label>
+        <input type="text" name="txtMon" id="txtMon" placeholder="Work Hours" >
+      </p>
+      <p>
+        <label for="tues">Tuesday Availability</label>
+        <input type="text" name="txtTues" id="txtTues" placeholder="Work Hours" >
+      </p>
+      <p>
+        <label for="wed">Wednesday Availability</label>
+        <input type="text" name="txtWed" id="txtWed" placeholder="Work Hours" >
+      </p>
+      <p>
+        <label for="thurs">Thursday Availability</label>
+        <input type="text" name="txtThurs" id="txtThurs" placeholder="Work Hours" >
+      </p>
+      <p>
+        <label for="fri">Friday Availability</label>
+        <input type="text" name="txtFri" id="txtFri" placeholder="Work Hours" >
+      </p>
+      <p>
+        <label for="sat">Saturday Availability</label>
+        <input type="text" name="txtSat" id="txtSat" placeholder="Work Hours" >
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <input type="submit" name="Submit" id="Submit" value="Submit">
+      </p>
+    </form>
+  </fieldset>
+</body>
+</html>
+
+<?php
+   if (isset($_POST['Submit']))
+   {
+      // database connection code
+      // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+      $servername = "localhost";
+      $username = "hashren9_james";
+      $password = "Mordor72!";
+      $databasename = "hashren9_myDB";
+
+      $con = mysqli_connect($servername, $username, $password, $databasename);
+
+      // Check if we can connect to server
+      if ($con === false) {
+        die('Could not connect to server: ' .mysqli_error());
+      }
+
+      $query = "SELECT fldName FROM tbl_userInfo";
+      $retval = mysqli_query($con, $query);
+
+      if ($retval === false) {
+        die('Could not get data: ' .mysqli_error());
+      }
+      else {
+        //echo "Data Retrieved: <br>";
+      }
+
+      // get the post records
+      $txtName = $_POST['txtName'];
+      $exists = "False";
+
+      while ($row = mysqli_fetch_assoc($retval)) {
+        if ($txtName == $row['fldName'])
+        {
+           $exists = "True";
+           break;
+        }
+      }
+
+      if ($exists == "True")
+      {
+        $txtSun = $_POST['txtSun'];
+        $txtMon = $_POST['txtMon'];
+        $txtTues = $_POST['txtTues'];
+        $txtWed = $_POST['txtWed'];
+        $txtThurs = $_POST['txtThurs'];
+        $txtFri = $_POST['txtFri'];
+        $txtSat = $_POST['txtSat'];
+
+        // database insert SQL code
+        //$sql = "INSERT INTO `tbl_availability` (`User`, `Sun`, `Mon`, `Tues`, `Wed`, `Thur`, `Fri`, `Sat`) VALUES ('$txtName', '$txtSun', '$txtMon', '$txtTues', '$txtWed', '$txtThurs', '$txtFri', '$txtSat')";
+
+        // insert in database
+        //$rs = mysqli_query($con, $sql);
+
+        if ($txtSun != "")
+        {
+          $q = "UPDATE tbl_availability SET Sun ='$txtSun' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+		
+        if ($txtMon != "")
+        {
+          $q = "UPDATE tbl_availability SET Mon ='$txtMon' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+		
+        if ($txtTues != "")
+        {
+          $q = "UPDATE tbl_availability SET Tues ='$txtTues' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+		
+        if ($txtWed != "")
+        {
+          $q = "UPDATE tbl_availability SET Wed ='$txtWed' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+		
+        if ($txtThurs != "")
+        {
+          $q = "UPDATE tbl_availability SET Thur ='$txtThurs' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+		
+        if ($txtFri != "")
+        {
+          $q = "UPDATE tbl_availability SET Fri ='$txtFri' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+		
+        if ($txtSat != "")
+        {
+          $q = "UPDATE tbl_availability SET Sat ='$txtSat' WHERE User='$txtName'";
+          $r = mysqli_query($con, $q);
+        }
+
+        
+      }
+      else
+      {
+        echo "Please enter a current employee name <Br>";
+      }
+
+
+      /*echo "$txtId <Br>";
+      echo "$txtUser <Br>";
+      echo "$txtPass <Br>";
+      echo "$txtName <Br>";
+      echo "$txtPosition <Br>";*/
+
+      mysqli_close($con);
+   }
+?>
