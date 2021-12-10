59ae0c8464d14c70f269714d336e9cb2d877d8ee
Alex Torres <89925046+AlexT0701@users.noreply.github.com>
2021-12-10 05:35:01 -0600

    Add files via upload

---
+++ hourly Login/Homepage/about.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HashRen Login</title>
+  <link rel="stylesheet" href="empAbout.css">
+</head>
+
+<body>
+<div class="header">
+  <a href="homepage.php" class="logo">HashRen Manage</a>
+  <div class="header-right">
+    <a href="hrAbout.php">About</a>
+    <a href="loginTest.php">Logout</a>
+  </div>
+</div>
+
+<div class="about-section">
+  <h1>About Us Page</h1>
+  <p>HashRen Management is an employee management web application with a built in employee login and HR portal.</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="james.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>James McMillan</h2>
+        <p class="title">Founder</p>
+        <p>Senior at Angelo State University majoring in Computer Science with a minor in Cyber Security.</p>
+        <p>jmcmillan@hashren.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="alex.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Alex Torres</h2>
+        <p class="title">Founder</p>
+        <p>Senior at Angelo State University majoring in Computer Science with a minor in Cyber Security.</p>
+        <p>atorres@hashren.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="contact.png" alt="John" style="width:100%">
+      <div class="container">
+        <h2>Null</h2>
+        <p class="title">Null</p>
+        <p>(Classification) at Angelo State University majoring in Computer Science with a minor in (minor here).</p>
+        <p>null@hashren.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+<p></p>
+<p></p>
+<p> Revision 1.00 </p>
