d9e4fd61a428833e7d28ae078c3658aa09ddf0c2
Alex Torres <89925046+AlexT0701@users.noreply.github.com>
2021-12-10 05:43:30 -0600

    Add files via upload

---
+++ hourly Login/Homepage/Calender/calender.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HashRen | Corporate Calendar</title>
+  <link rel="stylesheet" href="homepage.css">
+</head>
+
+<body>
+<div class="header">
+  <a href="homepage.php" class="logo">HashRen Manage</a>
+  <div class="header-right">
+    <a href="about.php">About</a>
+    <a href="loginTest.php">Logout</a>
+  </div>
+</div>
+
+<style>
+        table {
+            
+			border-collapse: collapse;
+            background: lightgrey;
+            color: black;
+        }
+          
+        th {
+			
+		}
+        td {
+			
+			text-align: right;
+			padding-right: 90px;
+            font-weight: bold;
+			border: 1px solid white;
+        }
+    </style>
+
+<h2 align="center" style="color: black;">
+        December 2021
+    </h2>
+    <br />
+  
+    <table bgcolor="lightgrey" align="center" 
+        cellspacing="20" cellpadding="40">
+  
+        <caption align="top">
+        </caption>
+  
+        <thead>
+            <tr>
+                <th style="color: white; background: red;">
+                    Sun</th>
+                <th style="color: white; background: red;">
+                    Mon</th>
+                <th style="color: white; background: red;">
+                    Tue</th>
+                <th style="color: white; background: red;">
+                    Wed</th>
+                <th style="color: white; background: red;">
+                    Thu</th>
+                <th style="color: white; background: red;">
+                    Fri</th>
+                <th style="color: white; background: red;">
+                    Sat</th>
+            </tr>
+        </thead>
+  
+        <tbody>
+            <tr>
+                <td style="opacity: .5;text-align: left">28</td>
+                <td style="opacity: .5;text-align: left">29</td>
+                <td style="opacity: .5;text-align: left">30</td>
+                <td style="text-align: left">1</td>
+                <td style="text-align: left">2</td>
+                <td style="text-align: left">3</td>
+                <td style="text-align: left">4</td>
+            </tr>
+            <tr></tr>
+            <tr>
+                <td style="text-align: left">5</td>
+                <td style="text-align: left">6</td>
+                <td style="text-align: left">7</td>
+                <td style="text-align: left">8 <br><u>Final <br> 9-10am<u></td>
+                <td style="text-align: left">9</td>
+                <td style="text-align: left">10 <br><u>Present! <br>@ 9am<u></td>
+                <td style="text-align: left">11 <br><u>Graduation<u></td>
+            </tr>
+            <tr>
+                <td style="text-align: left">12</td>
+                <td style="text-align: left">13</td>
+                <td style="text-align: left">14</td>
+                <td style="text-align: left">15</td>
+                <td style="text-align: left">16</td>
+                <td style="text-align: left">17</td>
+                <td style="text-align: left">18</td>
+            </tr>
+            <tr>
+                <td style="text-align: left">19</td>
+                <td style="text-align: left">20</td>
+                <td style="text-align: left">21</td>
+                <td style="text-align: left">22</td>
+                <td style="text-align: left">23</td>
+                <td style="text-align: left">24</td>
+                <td style="text-align: left">25</td>
+            </tr>
+            <tr>
+                <td style="text-align: left">26</td>
+                <td style="text-align: left">27</td>
+                <td style="text-align: left">28</td>
+                <td style="text-align: left">29</td>
+                <td style="text-align: left">30</td>
+                <td style="text-align: left">31</td>
+                <td style="opacity: .5;text-align: left">1</td>
+            </tr>
+            <tr>
+                <td style="opacity: .5;text-align: left">2</td>
+                <td style="opacity: .5;text-align: left">3</td>
+                <td style="opacity: .5;text-align: left">4</td>
+                <td style="opacity: .5;text-align: left">5</td>
+                <td style="opacity: .5;text-align: left">6</td>
+                <td style="opacity: .5;text-align: left">7</td>
+                <td style="opacity: .5;text-align: left">8</td>
+            </tr>
+        </tbody>
+    </table>
